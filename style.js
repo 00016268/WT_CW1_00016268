acf5b7a9884d49b9c1a9f7f6dd5c798f9641cec3
00016268 <wiut16268@gmail.com>
2022-11-24 03:04:20 +0500

    final update (functional Contact Us)

--- style.js
+++ style.js
@@ -1,8 +1,3 @@
-let search = document.querySelector('.search-box')
-
-document.querySelector('#search-icon').onclick = () => {
-    search.classList.toggle('active');
-}
 
 //get data
 
@@ -16,31 +11,48 @@ const errorNodes = document.querySelectorAll(".error");
 //validate data
 
 function validateForm(){
-
+       
     clearMessages();
+    let errorFlag = false;
 
-    if(nameInput.value.length < 1){
+    if (nameInput.value.length < 1){
         errorNodes[0].innerText = "Name cannot be blank";
         nameInput.classList.add("error-border");
+        errorFlag = true;
+    }
+
+    if(!emailIsValid(email.value)){
+        errorNodes[1].innerText = "Invalid Email Address";
+        email.classList.add("error-border");
+        errorFlag = true;
+
+    }
+
+    if(message.value.length < 1){
+        errorNodes[2].innerText = "Please Enter a Message";
+        email.classList.add("error-border");
+        errorFlag = true;
+    }
+
+    if(!errorFlag) { 
+        success.innerText = "Successfully Contacted, Please check your emails regularly for updates! ";
     }
 }
-
-
-// clear error / sucess messages 
-
-function clearMessages(){
-    for(let i = 0; i < errorNodes.length; i++){
-        errorNodes[i].innerText="";
-    }
-    nameInput.classList.remove("error-border");
-}
-
 
 
 function clearMessages(){
     for (let i = 0; i < errorNodes.length; i++){
         errorNodes[i].innerText = "";
     }
+    success.innerText = "";
+    nameInput.classList.remove("error-border");
+    email.classList.remove("error-border");
+    message.classList.remove("error-border");
+}
 
-    nameInput.classList.remove("error-border")
+
+//email validation
+function emailIsValid(email){
+    let pattern = /\S+@\S+\.\S+/;
+    return pattern.test(email)
 }
